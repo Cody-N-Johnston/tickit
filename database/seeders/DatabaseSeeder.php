62eaaca615413e0185d08bf096839a292b4957d8
Cody Johnston <cody.n.johnston@gmail.com>
2022-04-24 15:17:50 -0400

    Add default seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->run(GroupSeeder::class);
+        $this->run(UserSeeder::class);
+        $this->run(TicketStatusSeeder::class);
     }
 }
